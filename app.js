769b629c48255e9a60ee120968060ec7807c40d5
Francisco da Silva Filho <francisco.filho@nareia.com.uy>
2018-07-03 14:36:20 -0300

    Correcao retorno json

--- app.js
+++ app.js
@@ -28,8 +28,7 @@ app.get('/getAllNews', function(req, res) {
 				{titulo: 'Novidade', descricao: 'Pequena descrição 2', data_criacao: '10/12/2018', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com','www.equipepancho.com']},
 				{titulo: 'Novidade', descricao: 'Pequena descrição 3', data_criacao: '05/05/2018', url_imagens: [], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: []}
 				];	
-	res.json(news);
-    return;
+	return 	res.json(events);
 });
 
 //AllEnquetes
@@ -38,9 +37,8 @@ app.get('/getAllEnquetes', function(req, res) {
 				{titulo: 'Evento', descricao: 'Pequena descrição 1', data_inicio: '29/04/2018', data_fim: '30/04/2018', localizacao: 'Montevideo', valor: '100,00 reais', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 2', data_inicio: '10/12/2018', data_fim: '11/12/2018', localizacao: 'Limeira', valor: '100,50 reais', url_imagens: [], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com','www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 3', data_inicio: '05/05/2018', data_fim: '06/05/2018', localizacao: 'Campinas', valor: '35,00', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: []}
-				];	
-	res.json(events);
-    return;
+				];
+	return 	res.json(events);
 });
 
 //ReadMsgs
@@ -50,8 +48,7 @@ app.get('/readMsg', function(req, res) {
 				{titulo: 'Evento', descricao: 'Pequena descrição 2', data_inicio: '10/12/2018', data_fim: '11/12/2018', localizacao: 'Limeira', valor: '100,50 reais', url_imagens: [], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com','www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 3', data_inicio: '05/05/2018', data_fim: '06/05/2018', localizacao: 'Campinas', valor: '35,00', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: []}
 				];	
-	res.json(events);
-    return;
+	return res.json(events);
 });
 
 app.get('/replayPoll', function(req, res) {	
@@ -59,9 +56,8 @@ app.get('/replayPoll', function(req, res) {
 				{titulo: 'Evento', descricao: 'Pequena descrição 1', data_inicio: '29/04/2018', data_fim: '30/04/2018', localizacao: 'Montevideo', valor: '100,00 reais', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 2', data_inicio: '10/12/2018', data_fim: '11/12/2018', localizacao: 'Limeira', valor: '100,50 reais', url_imagens: [], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com','www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 3', data_inicio: '05/05/2018', data_fim: '06/05/2018', localizacao: 'Campinas', valor: '35,00', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: []}
-				];	
-	res.json(events);
-    return;
+				];
+    return res.json(events);
 });
 
 app.get('/newPoll', function(req, res) {	
@@ -70,8 +66,8 @@ app.get('/newPoll', function(req, res) {
 				{titulo: 'Evento', descricao: 'Pequena descrição 2', data_inicio: '10/12/2018', data_fim: '11/12/2018', localizacao: 'Limeira', valor: '100,50 reais', url_imagens: [], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com','www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 3', data_inicio: '05/05/2018', data_fim: '06/05/2018', localizacao: 'Campinas', valor: '35,00', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: []}
 				];	
-	res.json(events);
-    return;
+	
+    return res.json(events);;
 });
 
 app.get('/newMsg', function(req, res) {	
@@ -80,8 +76,7 @@ app.get('/newMsg', function(req, res) {
 				{titulo: 'Evento', descricao: 'Pequena descrição 2', data_inicio: '10/12/2018', data_fim: '11/12/2018', localizacao: 'Limeira', valor: '100,50 reais', url_imagens: [], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: ['www.equipepancho.com','www.equipepancho.com']},
 				{titulo: 'Evento', descricao: 'Pequena descrição 3', data_inicio: '05/05/2018', data_fim: '06/05/2018', localizacao: 'Campinas', valor: '35,00', url_imagens: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_videos: ['http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg','http://hortonhighschool.ca/wp-content/uploads/2017/10/News.jpg'], url_links: []}
 				];	
-	res.json(events);
-    return;
+	return res.json(events);	
 });
 
 
