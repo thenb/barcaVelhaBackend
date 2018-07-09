30e4c96e2b53dbc0651214d11fd8f6a479376173
Francisco da Silva Filho <francisco.filho@nareia.com.uy>
2018-07-09 14:19:53 -0300

    get all enquetes

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.get('/getAllMsgs', function(req, res) {
 //getAllEnquetes: Busca todas as enquetes
 app.get('/getAllEnquetes', function(req, res) {	
 	pool.getConnection(function(err, connection) {	
-		var string = 'select * from enquete';
+		var string = 'SELECT * FROM enquete LEFT JOIN enquete_x_usuario ON enquete.id = enquete_x_usuario.id_enquete UNION SELECT * FROM enquete RIGHT JOIN enquete_x_usuario ON enquete.id = enquete_x_usuario.id_enquete';
 		connection.query(string, function(err, data) {
 			if (err){
 				var error = {};
