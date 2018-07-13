91f59b9ecddbb0700fcfd4c625dbd9e1accd66eb
Francisco da Silva Filho <francisco.filho@nareia.com.uy>
2018-07-13 16:39:44 -0300

    onfire nullo

--- app.js
+++ app.js
@@ -204,6 +204,8 @@ app.post('/newMsg', function(req, res) {
 			req.body.on_fire = false;
 		}
 		
+		
+		
 		var string = 'insert into mensagem(descricao,data_criacao,data_evento,on_fire) values("'+req.body.descricao+'",now(),"'+req.body.data_evento+'",'+req.body.on_fire+')';
 		console.log(string);		
 		connection.query(string , function(err, data) {
