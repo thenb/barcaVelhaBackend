25b1d2ca2109f5f1aa9c188ea3260def5e997ce6
Francisco da Silva Filho <francisco.filho@nareia.com.uy>
2018-07-17 19:05:33 -0300

    enquetes por ordem de data

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.post('/getAllMsgs', function(req, res) {
 //getAllEnquetes: Busca todas as enquetes
 app.post('/getAllEnquetes', function(req, res) {	
 	pool.getConnection(function(err, connection) {	
-		var string = 'SELECT s.* FROM (SELECT * FROM enquete LEFT JOIN enquete_x_usuario  ON enquete.id = enquete_x_usuario.id_enquete where enquete_x_usuario.id_enquete is null or enquete_x_usuario.id_usuario = '+req.body.id_usuario+' UNION SELECT * FROM enquete RIGHT JOIN enquete_x_usuario ON enquete.id = enquete_x_usuario.id_enquete where enquete_x_usuario.id_enquete is null or enquete_x_usuario.id_usuario = '+req.body.id_usuario+' ) s ORDER BY s.respondido asc, s.data_criacao desc';
+		var string = 'SELECT s.* FROM (SELECT * FROM enquete LEFT JOIN enquete_x_usuario  ON enquete.id = enquete_x_usuario.id_enquete where enquete_x_usuario.id_enquete is null or enquete_x_usuario.id_usuario = '+req.body.id_usuario+' UNION SELECT * FROM enquete RIGHT JOIN enquete_x_usuario ON enquete.id = enquete_x_usuario.id_enquete where enquete_x_usuario.id_enquete is null or enquete_x_usuario.id_usuario = '+req.body.id_usuario+' ) s ORDER BY s.respondido asc, s.data_criacao desc  LIMIT 0,15';
 		console.log(string);
 		connection.query(string, function(err, data) {
 			if (err){
