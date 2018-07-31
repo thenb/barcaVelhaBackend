577e9115d616473d26d29f98b45e70059ed5f4fb
Francisco da Silva Filho <francisco.filho@nareia.com.uy>
2018-07-31 16:12:22 -0300

    enquete errada

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.post('/getAllMsgs', function(req, res) {
 app.post('/getAllEnquetes', function(req, res) {	
 	pool.getConnection(function(err, connection) {	
 		//var string = 'SELECT s.*, (select e.opcao_1_qtd from enquete as e where e.id = s.id) as opcao_1_qtd,(select e.opcao_2_qtd from enquete as e where e.id = s.id) as opcao_2_qtd,(select e.opcao_3_qtd from enquete as e where e.id = s.id) as opcao_3_qtd,(select e.opcao_4_qtd from enquete as e where e.id = s.id) as opcao_4_qtd  FROM (SELECT * FROM enquete LEFT JOIN enquete_x_usuario  ON enquete.id = enquete_x_usuario.id_enquete where enquete_x_usuario.id_enquete is null or enquete_x_usuario.id_usuario = '+req.body.id_usuario+' UNION SELECT * FROM enquete RIGHT JOIN enquete_x_usuario ON enquete.id = enquete_x_usuario.id_enquete where enquete_x_usuario.id_enquete is null or enquete_x_usuario.id_usuario = '+req.body.id_usuario+' ) s ORDER BY s.respondido asc, s.data_criacao desc  LIMIT 0,15';
-		var string = 'SELECT s.*, (select resposta from enquete_x_usuario where id_usuario = '+req.body.id_usuario+' and id_enquete = s.id) as resposta FROM enquete as s ORDER BY s.data_criacao desc  LIMIT 0,15';
+		var string = 'SELECT s.*, (select resposta from enquete_x_usuario where id_usuario = '+req.body.id_usuario+' and id_enquete = s.id) as resposta, , (select respondido from enquete_x_usuario where id_usuario = '+req.body.id_usuario+' and id_enquete = s.id) as respondido FROM enquete as s ORDER BY s.data_criacao desc  LIMIT 0,15';
 		
 		console.log(string);
 		connection.query(string, function(err, data) {
